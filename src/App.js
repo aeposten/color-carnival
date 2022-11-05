f82c23dd0fe7bdc1203daefdfa4269944735c20f
Amy <86963506+aeposten@users.noreply.github.com>
2022-11-04 17:51:58 -0700

    Removed uncommented code

--- src/App.js
+++ src/App.js
@@ -41,9 +41,9 @@ function App() {
   //   setWinningColor({...winningColor, ...winner})
   // }
 
-  // function resetGame() {
-  //   window.location.reload(false);
-  // }
+  function resetGame() {
+    window.location.reload(false);
+  }
 
   useEffect(() => {
     function startGame() {
